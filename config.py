cf132004e37b153f804d95cd6cd1c5208a4e6d76
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-07-01 15:50:38 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SUPABASE_DB_URL') or 'postgresql://cooper:cooper12@localhost:5432/skill_tracker'
+SQLALCHEMY_DATABASE_URI = os.environ.get('SUPABASE_DB_URL') or 'postgresql://postgres.iyxtfszywwrsefhzjwzj:[YOUR-PASSWORD]@aws-0-us-east-1.pooler.supabase.com:5432/postgres'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
