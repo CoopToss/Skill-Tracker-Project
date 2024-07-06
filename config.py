109c6b8b3e0bf81bcc9468288e3e545ed83e6ca8
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-07-06 12:20:25 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SUPABASE_DB_URL') or 'postgresql://postgres.iyxtfszywwrsefhzjwzj:Tebow1215Database@aws-0-us-east-1.pooler.supabase.com:5432/postgres'
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cooperwashere'
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SUPABASE_DB_URL') or 'postgresql://postgres.iyxtfszywwrsefhzjwzj:Tebow1215Database@aws-0-us-east-1.pooler.supabase.com:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError("No SUPABASE_DB_URL set for Flask application. Did you follow the setup instructions?")
