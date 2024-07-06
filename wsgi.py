d92eec481dbe66720fadfe5afcdff1eeeae8eab0
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-07-06 14:03:46 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,6 @@
 from app import create_app
-from config import Config
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = create_app()
-app.config.from_object(Config)
 
 if __name__ == "__main__":
     app.run()
