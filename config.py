2eabcb0ad2f811123a8bf7f31edcd7c20e419e9e
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-07-06 13:50:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cooperwashere'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SUPABASE_DB_URL') or 'postgresql://postgres.iyxtfszywwrsefhzjwzj:Tebow1215Database@aws-0-us-east-1.pooler.supabase.com:5432/postgres'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SUPABASE_DB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     if not SQLALCHEMY_DATABASE_URI:
